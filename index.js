f2e7a34649bfce02ce19b72cf2b01cc316ab85c8
Shakhawat-pro <ratulhossain2692004@gmail.com>
2024-08-16 23:14:49 +0600

    Mongodb setup done

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express()
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000
+
+
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t8njxkv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const productCollection = client.db('GadgetHub').collection('product')
+
+    app.get('/product', async(req, res)=>{
+        const result = await productCollection.find().toArray()
+        res.send(result)
+    })
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('GadgetHub running')
+  })
+  
+  app.listen(port, () => {
+    console.log('GadgetHub is running om port', port);
+  })
